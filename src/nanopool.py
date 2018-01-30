c6f3c10b6d50fe14cd4ef343720052b7f9bc65f3
zrmaker <renyuan.zhang@tusimple.ai>
2018-01-29 19:52:16 -0700

    nanopool+ind

--- src/nanopool.py
+++ src/nanopool.py
@@ -3,49 +3,58 @@ import urllib.request
 import json
 import matplotlib.pyplot as plt
 import datetime
-from src.technical_indicatiors import technical_indicators
+from technical_indicatiors import technical_indicators
 
 class nanopool():
     def __init__(self):
-        self.address='0x1b40335c8d882dad3fd3180f5ce457802c8a8cd8'
-        self.head='https://api.nanopool.org/v1/eth/'
+        self.eth_address='0x1b40335c8d882dad3fd3180f5ce457802c8a8cd8'
+        self.pasc_address = '86646.93a45081d188bbbb'
+        self.head='https://api.nanopool.org/v1/'
         self.agent={'User-Agent': 'Chrome/63.0.3239.84'}
         self.TI=technical_indicators()
         
-    def read(req):
+    def read(self, req):
         s = urllib.request.urlopen(req).read()
         return json.loads(s.decode("utf-8"))
         
-    def account_balance(self):
-        req=urllib.request.Request(self.head+'balance/'+self.address, \
-                    headers = self.agent)
-        ss = nanopool.read(req)
+    def account_balance(self, ticker):
+        if ticker == 'eth':
+            req=urllib.request.Request(self.head+ticker+'/balance/'+self.eth_address, headers = self.agent)
+        else:
+            req=urllib.request.Request(self.head+ticker+'/balance/'+self.pasc_address, headers = self.agent)
+        ss = self.read(req)
         return ss["data"]
     
-    def average_hashrate(self):
+    def average_hashrate(self, ticker):
         # 1h, 3h, 6h, 12h, 24h
-        req=urllib.request.Request(self.head+'avghashrate/'+self.address, \
-                    headers = self.agent)
-        ss = nanopool.read(req)
+        if ticker == 'eth':
+            req=urllib.request.Request(self.head+ticker+'/avghashrate/'+self.eth_address, headers = self.agent)
+        else:
+            req=urllib.request.Request(self.head+ticker+'/avghashrate/'+self.pasc_address, headers = self.agent)
+        ss = self.read(req)
         return np.fromiter(iter(ss["data"].values()),dtype=float)
     
-    def chart_data(self):
+    def chart_data(self, ticker):
         # 1h, 3h, 6h, 12h, 24h
-        req=urllib.request.Request(self.head+'hashratechart/'+self.address, \
-                    headers = self.agent)
-        ss = nanopool.read(req)
+        if ticker == 'eth':
+            req=urllib.request.Request(self.head+ticker+'/hashratechart/'+self.eth_address, headers = self.agent)
+        else:
+            req=urllib.request.Request(self.head+ticker+'/hashratechart/'+self.pasc_address, headers = self.agent)
+        ss = self.read(req)
         return ss["data"]
     
-    def history(self):
+    def history(self, ticker):
         # 1h, 3h, 6h, 12h, 24h
-        req=urllib.request.Request(self.head+'history/'+self.address, \
-                    headers = self.agent)
-        ss = nanopool.read(req)
+        if ticker == 'eth':
+            req=urllib.request.Request(self.head+ticker+'/history/'+self.eth_address, headers = self.agent)
+        else:
+            req=urllib.request.Request(self.head+ticker+'/history/'+self.pasc_address, headers = self.agent)
+        ss = self.read(req)
         return ss["data"]
     
-    def plot_chart(self):
-        data=nnp.chart_data()
-        data2=nnp.history()
+    def plot_chart(self, ticker):
+        data=self.chart_data(ticker)
+        data2=self.history(ticker)
         t,t2,shares,hr,hr2=[],[],[],[],[]
         for i in range(len(data)):
             t=np.append(t,datetime.datetime.fromtimestamp(data[i]['date']).strftime('%H:%M'))
@@ -61,7 +70,7 @@ class nanopool():
         shares=np.flip(shares,0)
         hr=np.flip(hr,0)
         hr2=np.flip(hr2,0)
-        mahr=self.TI.MA(hr2,12)
+        mahr=self.TI.MAlist(hr2,12)
         # print(len(t),len(shares),len(hr))
         f,(ax1,ax2)=plt.subplots(2,1)
         ax1.plot(t[-72:],hr[-72:],t2[-72:],hr2[-72:],t2[-72:],mahr[-72:])
@@ -69,5 +78,4 @@ class nanopool():
         plt.show()
         
 if __name__ == '__main__':
-    nnp=nanopool()
-    print(nnp.plot_chart())
+    print(nanopool().plot_chart('eth'))
--- src/technical_indicatiors.py
+++ src/technical_indicatiors.py
@@ -3,11 +3,54 @@ import pandas as pd
 
 class technical_indicators():
     def __init__(self):
-        self.close=0
+        self.alpha = .35
+        self.BBands_N = 20
+        self.BBands_K = 2
 
     def MA(self, adj_close, window_length):
-        result = pd.Series(adj_close).rolling(window= window_length).mean()
-        return result
+        if window_length > len(adj_close): window_length = len(adj_close)
+        ma_list = adj_close[-window_length:]
+        tmp = pd.Series(ma_list).rolling(window = window_length).mean().tolist()
+        return tmp[-1]
+
+    def MAlist(self, adj_close, window_length):
+        if window_length > len(adj_close): window_length = len(adj_close)
+        return pd.Series(adj_close).rolling(window = window_length).mean().tolist()
+
+    def EMA(self, adj_close, window_length):
+        if window_length > len(adj_close): window_length = len(adj_close)
+        self.alpha = 2 / (1 + window_length)
+        ema_list =  adj_close[-window_length:]
+        size_ = len(ema_list)
+        sum_ = .0
+        for i in range(size_):
+            sum_ += self.alpha * ema_list[i] * (1-self.alpha)**(size_-1-i) 
+        return sum_ / (1 - (1 - self.alpha)**size_)
+
+    def std_dev(self, adj_close, window_length):
+        if window_length > len(adj_close): window_length = len(adj_close)
+        std_list = adj_close[-window_length:]
+        return np.std(np.array(std_list))
+
+    def BBands(self, adj_close):
+        ma = self.MA(adj_close[-self.BBands_N:], self.BBands_N)
+        sigma = self.std_dev(adj_close[-self.BBands_N:], self.BBands_N)
+        lb = ma-self.BBands_K*sigma
+        ub = ma+self.BBands_K*sigma
+        percent_b = (adj_close[-1]-lb)/2/self.BBands_K/sigma
+        bandwidth = 2*self.BBands_K*sigma/ma
+        return lb, ub, percent_b, bandwidth
+
 
     def STOCH(self):
         self.close=0
+
+if __name__ == '__main__':
+    print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96],10))
+    print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96,14.47],10))
+    print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96,14.47,13.34],10))
+    print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96,14.47,13.34,13.77],10))
+    print(technical_indicators().EMA([29.18,34.9,33,26.59,28.45,26.39,22.8,23.27,22.19,24.9],10))
+    print(technical_indicators().MA([29.18,34.9,33,26.59,28.45,26.39,22.8,23.27,22.19,24.9],10))
+    print(technical_indicators().BBands([29.18,34.9,33,26.59,28.45,26.39,22.8,23.27,22.19,24.9,27.05,25.27,25.02,27.1,27.52,26.01,25.47,27.51,27.23,25.16]))
+    # print(technical_indicators().MA([22.81,23.09,22.91,23.23,22.83,23.05,23.02,23.29,23.41,23.49,24.6,24.63,24.51,23.73,23.31,23.53,23.06,23.25,23.12,22.8],10))
