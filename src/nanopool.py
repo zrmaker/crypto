5080f224213b335f20cde972796bdf4639e29221
zrmaker <renyuan.zhang@tusimple.ai>
2018-01-30 18:29:01 -0700

    progress0130

--- src/nanopool.py
+++ src/nanopool.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import urllib.request
 import json
@@ -41,7 +42,6 @@ class nanopool():
         else:
             req=urllib.request.Request(self.head+ticker+'/hashratechart/'+self.pasc_address, headers = self.agent)
         ss = self.read(req)
-        print(ss)
         return ss["data"]
     
     def history(self, ticker):
@@ -51,7 +51,6 @@ class nanopool():
         else:
             req=urllib.request.Request(self.head+ticker+'/history/'+self.pasc_address, headers = self.agent)
         ss = self.read(req)
-        print(ss)
         return ss["data"]
     
     def plot_chart(self, ticker):
@@ -59,35 +58,43 @@ class nanopool():
         data2=self.history(ticker)
         t,t2,shares,hr,hr2=[],[],[],[],[]
         for i in range(len(data)):
-            t=np.append(t,datetime.datetime.fromtimestamp(data[i]['date']).strftime('%H:%M'))
+            t=np.append(t,datetime.datetime.fromtimestamp(data[i]['date']).strftime('%m-%d %H:%M'))
             # t=np.append(t,data[i]['date'])
             shares=np.append(shares,data[i]['shares'])
             hr=np.append(hr,data[i]['hashrate']/1000.)
             
         for i in range(len(data2)):
-            t2 = np.append(t2, datetime.datetime.fromtimestamp(data2[i]['date']).strftime('%H:%M'))
+            t2 = np.append(t2, datetime.datetime.fromtimestamp(data2[i]['date']).strftime('%m-%d %H:%M'))
             hr2 = np.append(hr2, data2[i]['hashrate'])
-        t=np.flip(t,0)
-        t2=np.flip(t2,0)
-        shares=np.flip(shares,0)
-        hr=np.flip(hr,0)
-        hr2=np.flip(hr2,0)
-        mahr=self.TI.MAlist(hr2,12)
-        # print(len(t),len(shares),len(hr))
-        f,(ax1,ax2)=plt.subplots(2,1,figsize=(20, 8))
+        
+        f,(ax1,ax2)=plt.subplots(2,1,figsize=(20, 12))
         if ticker == 'eth':
-            print(hr2[-72:], mahr[-72:])
-            print(t[-72:], t2[-72:])
+            t=np.flip(t,0)
+            t2=np.flip(t2,0)
+            shares=np.flip(shares,0)
+            hr=np.flip(hr,0)
+            hr2=np.flip(hr2,0)
+            mahr=self.TI.MAlist(hr2,12)
             ax1.plot(t[-72:],hr[-72:],t2[-72:],hr2[-72:],t2[-72:],mahr[-72:])
+            ax1.legend(['Reported hashrate','Actual hashrate','2-hr moving average'])
         else:
-            print(hr2[-72:],mahr[-72:])
-            print(t[-72:],t2[-72:])
-            ax1.plot(hr2[-72:])
-            ax1.plot(mahr[-72:])
+            mahr=self.TI.MAlist(hr2,12)
+            ax1.plot(t2[-72:],hr2[-72:])
+            ax1.plot(t2[-72:],mahr[-72:])
+            ax1.legend(['Actual hashrate','2-hr moving average'])
         ax2.bar(t[-72:],shares[-72:])
         plt.setp(ax1.xaxis.get_majorticklabels(), rotation=45)
         plt.setp(ax2.xaxis.get_majorticklabels(), rotation=45)
         plt.show()
         
 if __name__ == '__main__':
-    print(nanopool().plot_chart('pasc'))
+    parser = argparse.ArgumentParser(description='ticker')
+    parser.add_argument(
+        'ticker',
+        type=str,
+        nargs='?',
+        default='eth',
+        help='Cryptocurrency name.'
+    )
+    args = parser.parse_args()
+    print(nanopool().plot_chart(args.ticker))
--- src/technical_indicatiors.py
+++ src/technical_indicatiors.py
@@ -6,6 +6,7 @@ class technical_indicators():
         self.alpha = .35
         self.BBands_N = 20
         self.BBands_K = 2
+        self.RSI_N = 14
 
     def MA(self, adj_close, window_length):
         if window_length > len(adj_close): window_length = len(adj_close)
@@ -41,16 +42,62 @@ class technical_indicators():
         bandwidth = 2*self.BBands_K*sigma/ma
         return lb, ub, percent_b, bandwidth
 
+    def change(self, adj_close):
+        return [x-y for x,y in zip(adj_close[1:],adj_close)]
+
+    def RSI(self, adj_close):
+        rs_list = adj_close[:self.RSI_N+1]
+        change_list = np.array(self.change(rs_list))
+        avg_gain = np.sum(change_list[change_list>0])/self.RSI_N
+        avg_loss = np.sum(change_list[change_list<0])/self.RSI_N
+        for i in range(self.RSI_N+1,len(adj_close)):
+            tmp = self.change([adj_close[i-1],adj_close[i]]).pop()
+            if tmp > 0:
+                avg_gain = (avg_gain*(self.RSI_N-1) + tmp) / self.RSI_N
+                avg_loss = avg_loss*(self.RSI_N-1)/self.RSI_N
+            elif tmp < 0:
+                avg_gain = avg_gain*(self.RSI_N-1)/self.RSI_N
+                avg_loss = (avg_loss*(self.RSI_N-1) + tmp) / self.RSI_N
+        RS = avg_gain/-avg_loss
+        RSI = 100 - 100 / (1 + RS)
+        if RSI > 70:
+            indicator = 'Overbought'
+        elif RSI < 30:
+            indicator = 'Oversold'
+        else:
+            indicator = ''
+        return RSI, indicator
+
+    def RSIlist(self, adj_close):
+        rs_list = adj_close[:self.RSI_N+1]
+        change_list = np.array(self.change(rs_list))
+        avg_gain = np.sum(change_list[change_list>0])/self.RSI_N
+        avg_loss = np.sum(change_list[change_list<0])/self.RSI_N
+        RSI = 100 - 100 / (1 + avg_gain/-avg_loss)
+        for i in range(self.RSI_N+1,len(adj_close)):
+            tmp = self.change([adj_close[i-1],adj_close[i]]).pop()
+            if tmp > 0:
+                avg_gain = (avg_gain*(self.RSI_N-1) + tmp) / self.RSI_N
+                avg_loss = avg_loss*(self.RSI_N-1)/self.RSI_N
+            elif tmp < 0:
+                avg_gain = avg_gain*(self.RSI_N-1)/self.RSI_N
+                avg_loss = (avg_loss*(self.RSI_N-1) + tmp) / self.RSI_N
+            RSI = np.append(RSI, 100 - 100 / (1 + avg_gain/-avg_loss))
+        return RSI
 
     def STOCH(self):
         self.close=0
 
 if __name__ == '__main__':
-    print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96],10))
-    print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96,14.47],10))
-    print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96,14.47,13.34],10))
-    print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96,14.47,13.34,13.77],10))
-    print(technical_indicators().EMA([29.18,34.9,33,26.59,28.45,26.39,22.8,23.27,22.19,24.9],10))
-    print(technical_indicators().MA([29.18,34.9,33,26.59,28.45,26.39,22.8,23.27,22.19,24.9],10))
-    print(technical_indicators().BBands([29.18,34.9,33,26.59,28.45,26.39,22.8,23.27,22.19,24.9,27.05,25.27,25.02,27.1,27.52,26.01,25.47,27.51,27.23,25.16]))
+    print(technical_indicators().RSI([44.34,44.09,44.15,43.61,44.33,44.83,45.10,45.42,45.84,46.08,45.89,46.03,45.61,46.28,46.28,46.00,46.03,46.41,46.22,45.64,46.21,46.25,45.71,46.45,45.78,45.35,44.03,44.18,44.22,44.57,43.42,42.66,43.13]))
+    import matplotlib.pyplot as plt
+    plt.plot(technical_indicators().RSIlist([44.34,44.09,44.15,43.61,44.33,44.83,45.10,45.42,45.84,46.08,45.89,46.03,45.61,46.28,46.28,46.00,46.03,46.41,46.22,45.64,46.21,46.25,45.71,46.45,45.78,45.35,44.03,44.18,44.22,44.57,43.42,42.66,43.13]))
+    plt.show()
+    # print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96],10))
+    # print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96,14.47],10))
+    # print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96,14.47,13.34],10))
+    # print(technical_indicators().EMA([11.98,12.18,12,12.31,12.19,12.35,12.4,12.64,13.05,13.96,14.47,13.34,13.77],10))
+    # print(technical_indicators().EMA([29.18,34.9,33,26.59,28.45,26.39,22.8,23.27,22.19,24.9],10))
+    # print(technical_indicators().MA([29.18,34.9,33,26.59,28.45,26.39,22.8,23.27,22.19,24.9],10))
+    # print(technical_indicators().BBands([29.18,34.9,33,26.59,28.45,26.39,22.8,23.27,22.19,24.9,27.05,25.27,25.02,27.1,27.52,26.01,25.47,27.51,27.23,25.16]))
     # print(technical_indicators().MA([22.81,23.09,22.91,23.23,22.83,23.05,23.02,23.29,23.41,23.49,24.6,24.63,24.51,23.73,23.31,23.53,23.06,23.25,23.12,22.8],10))
